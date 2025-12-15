095414d19a9ed776df411f1a04dfe846f12bd2de
LeandroCell <leandropaolicelli@gmail.com>
2025-12-15 14:56:44 +0100

    project

---
+++ randomNumber/randomNumber/main.java
@@ -0,0 +1,14 @@
+/**
+ * Dies ist die Hauptklasse eines Spiels. Sie enthält die Hauptmethode, die zum
+ * Starten des Spiels aufgerufen werden muss.
+ *
+ * @author Leandro Paolicelli
+ */
+public class main
+{
+    /** Das Spiel beginnt durch Aufruf dieser Methode. */
+    static void main()
+    {
+        // Durch den eigentlichen Code ersetzen
+    }
+}
