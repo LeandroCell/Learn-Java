01163451e2febc9737589bb0c40ef82571974dbb
LeandroWindows <leandropaolicelli@gmail.com>
2026-03-10 19:08:23 +0100

    loesung ueb09

--- Klausur/Uebung09/PI1Game/Npc.java
+++ Klausur/Uebung09/PI1Game/Npc.java
@@ -6,69 +6,69 @@ import static java.lang.Math.abs;
  *
  * @author Leandro Paolicelli
  */
-class Npc
+class Npc extends Actor
 {
-    private final GameObject npc;
+    private Player player;
     private RingBuffer stepsToFollow = null;
-    private final Field field;
     
-    Npc(GameObject npc, Field field) 
+    Npc(final int x, final int y, final int rotation, final String fileName, final Field field, final Player player) 
     {
-        this.npc = npc;
-        this.field = field;
+        super(x, y, rotation, fileName, field);
+        this.player = player;
     }
     
-    void act(final GameObject player)
+    @Override
+    void act()
     {
         if (stepsToFollow != null) {
             stepsToFollow.push(player.getRotation());
         }
         
-        if (npc.getRotation() == 0) {
-            npc.setLocation(npc.getX() + 1, npc.getY());
+        if (getRotation() == 0) {
+            setLocation(getX() + 1, getY());
         }
-        else if (npc.getRotation() == 1) {
-            npc.setLocation(npc.getX(), npc.getY() + 1);
+        else if (getRotation() == 1) {
+            setLocation(getX(), getY() + 1);
         }
-        else if (npc.getRotation() == 2) {
-            npc.setLocation(npc.getX() - 1, npc.getY());
+        else if (getRotation() == 2) {
+            setLocation(getX() - 1, getY());
         }
-        else if (npc.getRotation() == 3) {
-            npc.setLocation(npc.getX(), npc.getY() - 1);
+        else if (getRotation() == 3) {
+            setLocation(getX(), getY() - 1);
         }
-        npc.playSound("step");
+        playSound("step");
         
-        if (stepsToFollow != null && field.hasNeighbor(npc.getX(), npc.getY(), stepsToFollow.peek())) {
-            npc.setRotation(stepsToFollow.pop());
+        if (stepsToFollow != null && canWalk(stepsToFollow.peek())) {
+            setRotation(stepsToFollow.pop());
         }
         else {
             stepsToFollow = null;
                 
-            if(!field.hasNeighbor(npc.getX(), npc.getY(), npc.getRotation())) {
-               npc.setRotation((npc.getRotation() + 2) % 4);
+            if(!canWalk(getRotation())) {
+               setRotation((getRotation() + 2) % 4);
             }
             
-            if (npc.getRotation() == 0 && player.getX() > npc.getX() && player.getY() == npc.getY()
-                    || npc.getRotation() == 1 && player.getX() == npc.getX() && player.getY() > npc.getY() 
-                    || npc.getRotation() == 2 && player.getX() < npc.getX() && player.getY() == npc.getY()
-                    || npc.getRotation() == 3 && player.getX() == npc.getX() && player.getY() < npc.getY()) {
+            if (getRotation() == 0 && player.getX() > getX() && player.getY() == getY()
+                    || getRotation() == 1 && player.getX() == getX() && player.getY() > getY() 
+                    || getRotation() == 2 && player.getX() < getX() && player.getY() == getY()
+                    || getRotation() == 3 && player.getX() == getX() && player.getY() < getY()) {
                 
-                final int distance = max(abs(player.getX() - npc.getX()), 
-                        abs(player.getY() -npc.getY()));
+                final int distance = max(abs(player.getX() - getX()), 
+                        abs(player.getY() -getY()));
                 
                 if (distance <= 4) {
                     stepsToFollow = new RingBuffer(distance);
                     
                     for (int i = 1; i < distance; ++i) {
-                        stepsToFollow.push(npc.getRotation());
+                        stepsToFollow.push(getRotation());
                     }
                 }
             }
         }
         
-        if (npc.getX() == player.getX() && npc.getY() == player.getY()) {
+        if (getX() == player.getX() && getY() == player.getY()) {
             player.setVisible(false);
-            npc.playSound("go-away");
+            playSound("go-away");
         }  
     }
 }
--- Klausur/Uebung09/PI1Game/PI1Game.java
+++ Klausur/Uebung09/PI1Game/PI1Game.java
@@ -1,5 +1,5 @@
-// Importieren der VK_*-Tastenkonstanten
-import static java.awt.event.KeyEvent.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Dies ist die Hauptklasse eines Spiels. Sie enthält die Hauptmethode, die zum
@@ -30,40 +30,17 @@ abstract class PI1Game extends Game
         new GameObject(3, 3, 4, "water-l");
         new GameObject(4, 3, 0, "water-i");
         
-        final GameObject player = new GameObject(0, 3, 0, "woman");
-        final Npc npc1 = new Npc(new GameObject(3, 2, 2, "child"), field);
-        final Npc npc2 = new Npc(new GameObject(0, 1, 0, "laila"), field);
-        final Npc npc3 = new Npc(new GameObject(1, 0, 2, "claudius"), field);
+        final Player player = new Player(0, 3, 0, field);
+        final List<Actor> actors = new ArrayList<>();
+        actors.add(player);
+        actors.add(new Npc(3, 2, 2, "child", field, player));
+        actors.add(new Npc(0, 1, 0, "laila", field, player));
+        actors.add(new Npc(1, 0, 2, "claudius", field, player));
         
         while(player.isVisible()) {
-            final int key = getNextKey();
-            if (key == VK_RIGHT && field.hasNeighbor(player.getX(), player.getY(), 0)) {
-                player.setRotation(0);
-                player.setLocation(player.getX() + 1, player.getY());
-                playSound("step");
+            for (final Actor actor : actors) {
+                actor.act();
             }
-            else if (key == VK_DOWN && field.hasNeighbor(player.getX(), player.getY(), 1)) {
-                player.setRotation(1);
-                player.setLocation(player.getX(), player.getY() + 1);
-                playSound("step");
-            }
-            else if (key == VK_LEFT && field.hasNeighbor(player.getX(), player.getY(), 2)) {
-                player.setRotation(2);
-                player.setLocation(player.getX() - 1, player.getY());
-                playSound("step");
-            }
-            else if (key == VK_UP && field.hasNeighbor(player.getX(), player.getY(), 3)) {
-                player.setRotation(3);
-                player.setLocation(player.getX(), player.getY() - 1);
-                playSound("step");
-            }
-            else {
-                playSound("error");
-                continue;
-            }
-            npc1.act(player);
-            npc2.act(player);
-            npc3.act(player);
         }
     }
 }
